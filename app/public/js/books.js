724737c90439bcd6d0935ec86f549428328a8eeb
ryank631 <ryank0516@gmail.com>
2021-10-28 21:55:08 -0400

    fixed edit and delete errors

--- app/public/js/books.js
+++ app/public/js/books.js
@@ -1,56 +1,119 @@
-const Books = {
+const book = {
     data() {
-      return {
+    return {
         books: [],
-        selectedBook: null,
-        bookForm: {}
-        }
+        booksform: {},
+        selectedBook: null
+    }
     },
     computed: {},
     methods: {
-        fetchBookData() {
-            fetch('api/books/index.php')
-            .then( response => response.json() )
-            .then( (responseJson) => {
-                console.log(responseJson);
-                this.books = responseJson;
-            })
-            .catch( (err) => {
+    prettyDollar(n) {
+        const d = new Intl.NumberFormat("en-US").format(n);
+        return "$ " + d;
+    },
+    fetchBooksData() {
+        fetch("/api/books/")
+        .then(response => response.json())
+        .then((responseJson) => {
+            console.log(responseJson);
+            this.books = responseJson;
+        })
+            .catch( err => {
                 console.error(err);
             })
-        },
-    selectBook(b) {
-        if (b == this.selectedBook) {
-            return;
-        }
-        this.selectedBook = b;
-        this.books = [];
-        this.fetchBookData(this.selectedBook);
     },
-    postNewBook(evt) {
-        // this.bookForm.title = this.selectedBook.title;
-        // console.log("Posting!", this.bookForm);
-        alert("Created!");
-
-        fetch('api/books/create.php', {
+    postBook(evt){
+        console.log ("Test:", this.selectedBook);
+        if (this.selectedBook) {
+            this.postEditBook(evt);
+        } else {
+            this.postNewBook(evt);
+        }
+    },
+    postEditBook(evt) {
+        this.booksform.id = this.selectedBook.id;
+    
+        console.log("Editing!", this.booksform);
+    
+        fetch('api/books/update.php', {
             method:'POST',
-            body: JSON.stringify(this.bookForm),
+            body: JSON.stringify(this.booksform),
+            headers: {
+              "Content-Type": "application/json; charset=utf-8"
+            }
+          })
+          .then( response => response.json() )
+          .then( json => {
+            console.log("Returned from post:", json);
+            
+            this.books = json;
+            
+            
+            this.handleResetEdit();
+          });
+      },
+    postNewBook(evt) {
+        console.log("Posting!", this.booksform);
+        
+    
+        fetch('api/books/create.php', {
+            method: 'POST',
+            body: JSON.stringify(this.booksform),
             headers: {
                 "Content-Type": "application/json; charset=utf-8"
             }
         })
-        .then( response => response.json())
-        .then( json => {
-            console.log("Returned from post:", json);
-            this.books = json;
-            this.bookForm = {};
-        });
+            .then(response => response.json())
+            .then( json => {
+                console.log("Returned from post:", json);
+                this.books = json;
+    
+                
+                this.booksform = {};
+                this.handleResetEdit();
+            });
+    
+        },
+    postDeleteBook(b) {
+        if (!confirm("Are you sure you want to delete the book?")) {
+            return;
+        }
+    
+        console.log("Delete!", b);
+    
+        fetch('api/books/delete.php', {
+            method: 'POST',
+            body: JSON.stringify(b),
+            headers: {
+                "Content-Type": "application/json; charset=utf-8"
+            }
+            })
+            .then(response => response.json())
+            .then(json => {
+                console.log("Returned from post:", json);
+                
+                this.books = json;
+    
+                
+                this.handleResetEdit();
+            });
+    },
+    
+    handleEditBook(book) {
+    this.selectedBook = book;
+    this.booksform = Object.assign({}, this.selectedBook);
+    },
+    handleResetEdit() {
+    this.selectedBook = null;
+    this.booksform = {};
     }
-  },
+    },
+    
     created() {
-        this.fetchBookData();
-     
+    this.fetchBooksData();
+    
+    }   
     }
-}
-  
-Vue.createApp(Books).mount('#booksApp');
+    
+    Vue.createApp(book).mount('#booksApp');
